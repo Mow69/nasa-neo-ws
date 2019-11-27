974546c91c6855d5f5cf72106c87e45cdc3f54b2
Mouaz SAADAOUI <msa@cnam-lyon.peda>
2019-11-27 09:42:58 +0100

    correction

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import NeoWsContainer from "./NeoWs/NeoWsContainer";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <NeoWsContainer />
+    </>
+  );
+}
+
+export default App;
